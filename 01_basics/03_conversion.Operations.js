8ef038ccc56e60d961980668720dd5067e79aa58
XTREME HASTAR <105353074+XTREMEHASTAR@users.noreply.github.com>
2024-12-19 07:33:19 +0000

    Coversion of datatypes

---
+++ 01_basics/03_conversion.Operations.js
@@ -0,0 +1,28 @@
+let score = "33abs"
+
+ //console.log(typeof score);
+ //console.log(typeof(score));
+
+ let valueInNumber = Number(score)
+ //console.log(typeof valueInNumber);
+ //console.log(valueInNumber);
+
+
+ // "33" => 33
+ //"33abc" = NaN
+ //true =>1/False=>0
+
+ let isLoggedIn = 1
+
+ let booleaIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleaIsLoggedIn);
+
+ // 1=> true; 0=>false
+ // "" => false
+ // "veer" => true
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
