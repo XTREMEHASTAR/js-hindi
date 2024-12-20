d135f0331712967e4f914eceb6285cf26b238104
XTREME HASTAR <105353074+XTREMEHASTAR@users.noreply.github.com>
2024-12-20 07:46:10 +0000

    Date and time

---
+++ 01_basics/07_date_and_time.js
@@ -0,0 +1,25 @@
+// dates
+
+let myDate = new Date()
+//console.log(myDate.toString());
+//console.log(myDate.toDateString());
+//console.log(myDate.toISOString());
+//console.log(myDate.toLocaleDateString());
+//console.log(myDate.toLocaleTimeString());
+//console.log(myDate.toTimeString());
+//console.log(myDate.toUTCString());
+//console.log(typeof myDate);
+
+//let myCreatedDate = new Date(2024, 0, 22)
+//let myCreatedDate = new Date(2024, 0, 22, 5, 3)
+//let myCreatedDate = new Date("2024-02-15")
+//let myCreatedDate = new Date("01-14-2024")
+//console.log(myCreatedDate.toDateString());
+
+//let myTimeStamp = Date.now()
+//console.log(myTimeStamp);
+//console.log(Math.floor(Date.now()/1000));
+
+//let newDate = new Date()
+//console.log(newDate);
+//console.log(newDate.getHours())
