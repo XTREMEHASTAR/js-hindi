78330e09e644b24cc33cdd4dc98ee4dc0c186bd7
XTREME HASTAR <105353074+XTREMEHASTAR@users.noreply.github.com>
2024-12-20 07:19:53 +0000

    Maths and num

---
+++ 01_basics/06_num_and_maths.js
@@ -0,0 +1,33 @@
+const score = 400
+//console.log(score);
+
+const balance = new Number(100)
+//console.log(balance);
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(3));
+
+const otherNumber = 23.565856
+
+//console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+//console.log(hundreds.toLocaleString('en-IN'));
+
+
+////////////////////////////////////////
+
+//console.log(Math);
+//console.log(Math.abs(-11));
+//console.log(Math.round(4.9));
+//console.log(Math.ceil(4.1));
+//console.log(Math.floor(4.9));
+//console.log(Math.min(1,8,9,6,5,4,3));
+//console.log(Math.max(4,5,3,8,9));
+
+console.log((Math.random()*10) +1);
+
+const min = 10
+const max = 40
+
+console.log(Math.floor(Math.random() *  ( max - min + 1)) + min)
