8fe3845fe7b8f567e950a3b9e2e6b1cb2fac08b1
XTREME HASTAR <105353074+XTREMEHASTAR@users.noreply.github.com>
2025-01-05 10:12:57 +0000

    Loops

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -27,3 +27,21 @@ for (let index = 0; index < myArray.length; index++) {
   console.log(element);
   
 }
+
+// break and continue
+
+// for (let index = 1; index <= 20; index++) {
+//   if (index == 5) {
+//     console.log(`Detect 5`);
+//     break
+//   }
+//   console.log(`Value of i  is ${index}`);
+// }
+
+for (let index = 1; index <= 20; index++) {
+  if (index == 5) {
+    console.log(`Detect 5`);
+    continue
+  }
+  console.log(`Value of i  is ${index}`);
+}
